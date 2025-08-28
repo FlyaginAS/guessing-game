249e539aefef89b78dfd45c393b2668f3746d907
FlyaginAS <rus.reg.36@gmail.com>
2025-08-28 17:15:15 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -11,19 +11,97 @@ class GuessingGame {
   setRange(min, max) {
     this.min = min;
     this.max = max;
+    this.mid = Math.round((this.min + this.max) / 2);
   }
 
   guess() {
-    this.mid = Math.round(this.max - this.min) / 2;
+    // ошибка округления...
+    // 279,280
+    // 278, 279
+    // this.mid = Math.round((this.min + this.max) / 2);
+    // (278+279)/2= 278.5 round=> 279
+    // (279+280)/2=279.5 round=>280
+    // this.mid = Math.round((this.max - this.min) / 2) + this.min;
+    // (279-278)/2=0.5 round=>1, 1+278=279
+    // (280-279)/2=0.5 round=>1, 1+279=280
+    // console.log(this.mid);
+
+    // ерунда какя-то, попробую костыль
+    // this.mid = this.min + Math.floor((this.max - this.min) / 2);
+    // if ((this.min + this.max) % 2 !== 0) {
+    //   this.mid += 1;
+    // }
+
+    this.mid = Math.round((this.max + this.min) / 2);
     return this.mid;
   }
+  // ошибка была в  this.min\max = this.mid+1,
 
   lower() {
     this.max = this.mid;
-    this.mid = Math.round(this.max - this.min) / 2;
   }
 
-  greater() {}
+  greater() {
+    this.min = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
+
+// const number = 409;
+// const game = new GuessingGame();
+// game.setRange(0, 4048);
+
+// let result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// console.log('*********');
+// console.log(result);
+
+//! *****************************
+// const number = 279;
+// const game = new GuessingGame();
+// game.setRange(0, 3445);
+
+// let result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// console.log(result);
