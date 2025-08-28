9711c7a49d1755fc6cc190496d70d56d78cfcbd1
FlyaginAS <rus.reg.36@gmail.com>
2025-08-28 16:01:24 +0600

    init: start Guess task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    // this.left = 0;
+    // this.right = 0;
+    this.mid = 0;
+    // this.range = [];
+    this.min = 0;
+    this.max = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.mid = Math.round(this.max - this.min) / 2;
+    return this.mid;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.mid;
+    this.mid = Math.round(this.max - this.min) / 2;
+  }
+
+  greater() {}
 }
 
 module.exports = GuessingGame;
